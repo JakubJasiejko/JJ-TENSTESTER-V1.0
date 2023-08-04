a9c0fbb560004dfb121122e16ce4827d0e49fd39
JakubJasiejko <140650871+JakubJasiejko@users.noreply.github.com>
2023-08-04 17:03:40 +0200

    Add tensile strenght and interface S0 value

--- software/Software.m
+++ software/Software.m
@@ -13,8 +13,11 @@
 %   MATLAB Software for control tester device and analising data          %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
  function Control()
+  
+ crossSectionalArea = 0;
 
-Device = serialport("COM7", 9600); %% Wybierz właściwy port COM, do którego jest podłączona płytka Arduino.
+ 
+ Device = serialport("COM7", 9600); %% Wybierz właściwy port COM, do którego jest podłączona płytka Arduino.
 
 % Tworzenie przycisków
 figure(1);
@@ -24,10 +27,52 @@ button3 = uicontrol('Style', 'pushbutton', 'String', 'YOUNG MODULUS TEST', 'Posi
 button4 = uicontrol('Style', 'pushbutton', 'String', 'UPWARD MOVEMENT', 'Position', [10 80 120 30], 'Callback', @startUpwardMovement);
 button5 = uicontrol('Style', 'pushbutton', 'String', 'DOWNWARD MOVEMENT', 'Position', [10 40 140 30], 'Callback', @startDownwardMovement);
 resetButton = uicontrol('Style', 'pushbutton', 'String', 'RESET PLOT', 'Position', [10 10 100 30], 'Callback', @resetPlot);
+disableButton = uicontrol('Style', 'pushbutton', 'String', 'TURN OFF', 'Position', [10 240 100 30], 'Callback', @turnOff);
+
+fig = figure('Position', [100, 100, 300, 150], 'Name', 'Enter values', 'NumberTitle', 'off', 'MenuBar', 'none');
+
+prompt1 = uicontrol('Style', 'text', 'Position', [20, 100, 100, 20], 'String', 'first side lenght [mm]:');
+edit1 = uicontrol('Style', 'edit', 'Position', [120, 100, 100, 20]);
+
+prompt2 = uicontrol('Style', 'text', 'Position', [20, 70, 100, 20], 'String', 'second side lenght [mm]:');
+edit2 = uicontrol('Style', 'edit', 'Position', [120, 70, 100, 20]);
+
+confirmButton = uicontrol('Style', 'pushbutton', 'Position', [100, 0, 100, 30], 'String', 'Confirm', 'Callback', @confirmCallback);
+
+resultText = uicontrol('Style', 'text', 'Position', [20, 40, 300, 20], 'String', '', 'HorizontalAlignment', 'left');
+
+
 
 measurmentData = [];
 displacementData = [];
 maxForceValue = 0;
+
+    function confirmCallback(~, ~)
+        % cross-sectional area- So
+        
+        a = str2double(get(edit1, 'String')); %first side lenght [mm]
+        b = str2double(get(edit2, 'String')); %second side lenght [mm]
+
+        % Sprawdzanie, czy wprowadzone wartości są poprawne
+        if ~isnan(a) && ~isnan(b)
+            % Przypisywanie wartości do zmiennych w kodzie
+            sideLenghtA = a;
+             sideLenghtB = b;
+             crossSectionalArea = a * b;
+            
+            % Wyświetlanie przypisanych wartości
+           disp(['A side lenght: ', num2str(sideLenghtA), '[mm]']);
+           disp(['B side lenght: ', num2str(sideLenghtB), '[mm]']);
+           disp('Cross sectional area:')
+           disp(num2str(crossSectionalArea));
+           disp('[mm^2]')
+            % Zamykanie okna GUI
+            close(fig);
+        else
+            % Wyświetlanie komunikatu o błędzie
+            errordlg('Wprowadź poprawne wartości liczbowe.', 'Błąd', 'modal');
+        end
+    end
 
 % Funkcje dla przycisków
     function startSlowTest(~, ~)
@@ -58,6 +103,10 @@ maxForceValue = 0;
         clf(2,"reset"); % Wyczyszczenie aktualnego wykresu
         figure(2); % Przełączenie się do nowego okna
     end
+     
+     function turnOff(~, ~)
+         clc;clear;close all;
+     end
 
 while true
     data = readline(Device);
@@ -76,10 +125,11 @@ while true
 
     % Aktualizacja największej wartości siły
     maxForceValue = max(maxForceValue, max(measurmentData));
-    
+    maxTensStrenght = maxForceValue / crossSectionalArea;
     % Dodanie tekstu z największą wartością siły na wykresie
-    text(0.7, 0.9, ['Max Force: ' num2str(maxForceValue)], 'Units', 'normalized');
-
+    
+text(0.7, 0.9, ['Max Force: ' num2str(maxForceValue)], 'Units', 'normalized');
+text(0.7, 0.8, ['Tensile strength: ' num2str(maxTensStrenght)], 'Units', 'normalized');
     drawnow; % Aktualizacja wykresu
 
 end
@@ -87,3 +137,4 @@ end
 end
 
 
+%%error powoduje niewyswietlanie wykresu!!!! error num2str
