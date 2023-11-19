f7e7356429316cdc34b265847b785a7286354d2b
JakubJasiejko <140650871+JakubJasiejko@users.noreply.github.com>
2023-11-19 14:38:13 +0100

    added encoder microcontroller fw

---
+++ firmware/encoder_microcontroller_firmware/src/main.cpp
@@ -0,0 +1,48 @@
+#include <arduino.h>
+
+static const int encoderPinA = 3;
+static const int encoderPinB = 2;
+
+bool encoderPinBState = false;
+
+int encoderPulses = 0;
+
+void interruptService(){
+encoderPinBState = digitalRead(encoderPinB);
+if (encoderPinBState == LOW){
+  encoderPulses++;
+  Serial.println(encoderPulses);
+}
+else if(encoderPinBState == HIGH){
+  encoderPulses--;
+  Serial.println(encoderPulses);
+}
+
+
+
+}
+
+void setup(){
+
+ // Inicjalizacja portu szeregowego dla monitora szeregowego (do celów diagnostycznych)
+  Serial.begin(9600);
+  Serial.println("ready");
+  // Konfiguracja pinu przerwań jako wejście
+  pinMode(encoderPinA, INPUT);
+  pinMode(encoderPinB, INPUT);
+
+  // Włączenie pull-up na pinie przerwań
+  digitalWrite(encoderPinA, HIGH);
+  digitalWrite(encoderPinB, HIGH);
+
+  // Konfiguracja przerwania na narastające zbocze (RISING)
+  attachInterrupt(digitalPinToInterrupt(encoderPinA), interruptService, RISING);
+
+
+
+}
+
+void loop(){
+
+  
+}
